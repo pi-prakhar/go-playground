7e63bcb0c22f9115c3c28ea1676c2d2b345bad32
Prakhar Awasthi <pawasthi961@gmail.com>
2024-06-25 23:08:50 +0530

    fixed find min in rotated sorted array

--- Problem-Solving/Problem19/main.go
+++ Problem-Solving/Problem19/main.go
@@ -7,21 +7,43 @@ leetcode -> Find Minimum in Rotated Sorted Array -> medium
 Tags:
 array, binary search
 **/
-func findMin(nums []int) int {
-	l := len(nums)
-	left := 0
-	right := l - 1
-	mid := (left + right) / 2
-	gap := mid - left
 
-	for !((nums[left] <= nums[mid]) && (nums[mid] <= nums[right])) {
-		if (nums[mid] >= nums[left]) && (nums[left] >= nums[right]) {
-			left = right
+func findMin(nums []int) int {
+	l := 0
+	h := len(nums) - 1
+	mid := 0
+	target := nums[l]
+	index := l
+
+	for l <= h {
+		mid = (l + h) / 2
+		if nums[mid] >= target {
+			l = mid + 1
 		} else {
-			left = mid
+			target = nums[mid]
+			index = mid
+			h = mid - 1
 		}
-		right = (left + l - 1) % l
-		mid = (left + gap) % l
 	}
-	return nums[left]
+
+	return nums[index]
 }
+
+// func findMin(nums []int) int {
+// 	l := len(nums)
+// 	left := 0
+// 	right := l - 1
+// 	mid := (left + right) / 2
+// 	gap := mid - left
+
+// 	for !((nums[left] <= nums[mid]) && (nums[mid] <= nums[right])) {
+// 		if (nums[mid] >= nums[left]) && (nums[left] >= nums[right]) {
+// 			left = right
+// 		} else {
+// 			left = mid
+// 		}
+// 		right = (left + l - 1) % l
+// 		mid = (left + gap) % l
+// 	}
+// 	return nums[left]
+// }
