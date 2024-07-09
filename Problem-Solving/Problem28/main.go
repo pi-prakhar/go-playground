94cc14142d3422be6978a3d630e0ff73df70fe23
Prakhar Awasthi <pawasthi961@gmail.com>
2024-07-09 23:13:59 +0530

    solved partition

--- Problem-Solving/Problem28/main.go
+++ Problem-Solving/Problem28/main.go
@@ -9,5 +9,35 @@ arrays, strings, recursion, backtracking
 
 **/
 func partition(s string) [][]string {
+	var list [][]string
+	solve(&s, &list, []string{}, 0)
+	return list
 
+}
+
+func solve(s *string, list *[][]string, temp []string, start int) {
+	if start == len(*s) {
+		copyTemp := make([]string, len(temp))
+		copy(copyTemp, temp)
+		(*list) = append((*list), copyTemp)
+	} else {
+		for i := start; i < len(*s); i++ {
+			if isPalindrome(s, start, i) {
+				temp = append(temp, (*s)[start:i+1])
+				solve(s, list, temp, i+1)
+				temp = temp[:len(temp)-1]
+			}
+		}
+	}
+}
+
+func isPalindrome(s *string, start int, end int) bool {
+	for start <= end {
+		if (*s)[start] != (*s)[end] {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
 }
--- Problem-Solving/Problem28/main_test.go
+++ Problem-Solving/Problem28/main_test.go
@@ -1,5 +1,50 @@
 package main
 
-func Test_partition(s string) [][]string {
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
 
+func Test_partition(t *testing.T) {
+	tests := []struct {
+		s    string
+		want [][]string
+	}{
+		{
+			s:    "aab",
+			want: [][]string{{"a", "a", "b"}, {"aa", "b"}},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			got := partition(test.s)
+			sortSubsets(got)
+			sortSubsets(test.want)
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("For input %v, expected %v, but got %v", test.s, test.want, got)
+			}
+		})
+	}
+}
+
+func sortSubsets(subsets [][]string) {
+	// Sort individual subsets
+	for _, subset := range subsets {
+		sort.Strings(subset)
+	}
+
+	// Sort the list of subsets
+	sort.Slice(subsets, func(i, j int) bool {
+		if len(subsets[i]) != len(subsets[j]) {
+			return len(subsets[i]) < len(subsets[j])
+		}
+		for k := range subsets[i] {
+			if subsets[i][k] != subsets[j][k] {
+				return subsets[i][k] < subsets[j][k]
+			}
+		}
+		return false
+	})
 }
