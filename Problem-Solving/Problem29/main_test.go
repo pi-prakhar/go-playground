dc8735899fc73fd73d3177aa3c72b074635064ae
Prakhar Awasthi <pawasthi961@gmail.com>
2024-07-17 22:42:01 +0530

    wrote tests for word searcg

--- Problem-Solving/Problem29/main_test.go
+++ Problem-Solving/Problem29/main_test.go
@@ -1,1 +1,80 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestExist(t *testing.T) {
+	tests := []struct {
+		board [][]byte
+		word  string
+		want  bool
+	}{
+		{
+			board: [][]byte{
+				{'A', 'B', 'C', 'E'},
+				{'S', 'F', 'C', 'S'},
+				{'A', 'D', 'E', 'E'},
+			},
+			word: "ABCCED",
+			want: true,
+		},
+		{
+			board: [][]byte{
+				{'A', 'B', 'C', 'E'},
+				{'S', 'F', 'C', 'S'},
+				{'A', 'D', 'E', 'E'},
+			},
+			word: "SEE",
+			want: true,
+		},
+		{
+			board: [][]byte{
+				{'A', 'B', 'C', 'E'},
+				{'S', 'F', 'C', 'S'},
+				{'A', 'D', 'E', 'E'},
+			},
+			word: "ABCB",
+			want: false,
+		},
+		{
+			board: [][]byte{
+				{'A'},
+			},
+			word: "A",
+			want: true,
+		},
+		{
+			board: [][]byte{
+				{'A'},
+			},
+			word: "B",
+			want: false,
+		},
+		{
+			board: [][]byte{
+				{'A', 'B'},
+				{'C', 'D'},
+			},
+			word: "ABCD",
+			want: false,
+		},
+		{
+			board: [][]byte{
+				{'A', 'D'},
+				{'C', 'B'},
+			},
+			word: "ACBD",
+			want: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.word, func(t *testing.T) {
+			got := exist(tt.board, tt.word)
+			if got != tt.want {
+				t.Errorf("exist(%v, %s) = %v; want %v", tt.board, tt.word, got, tt.want)
+			}
+		})
+	}
+}
