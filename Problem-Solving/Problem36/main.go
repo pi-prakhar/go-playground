3a05c41a05856edcdc0ac50db71cd7ce47e9a31e
Prakhar Awasthi <pawasthi961@gmail.com>
2024-08-04 13:26:27 +0530

    wrote an optimised version

--- Problem-Solving/Problem36/main.go
+++ Problem-Solving/Problem36/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "strconv"
+import (
+	"bytes"
+	"strconv"
+)
 
 func countAndSay(n int) string {
 	if strconv.Itoa(n) == "1" {
@@ -27,4 +30,38 @@ func getRLEOfNumber(s string) string {
 
 	retN = retN + strconv.Itoa(tempCount) + string(tempN)
 	return retN
+}
+
+func countAndSayOptimized(n int) string {
+	if n == 1 {
+		return "1"
+	}
+
+	prev := []byte("1")
+	for i := 2; i <= n; i++ {
+		prev = getRLEOfNumberOptimized(prev)
+	}
+
+	return string(prev)
+}
+
+func getRLEOfNumberOptimized(s []byte) []byte {
+	var result bytes.Buffer
+	result.Grow(len(s) * 2) // Preallocate memory
+
+	count := 1
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			count++
+		} else {
+			result.WriteString(strconv.Itoa(count))
+			result.WriteByte(s[i-1])
+			count = 1
+		}
+	}
+
+	result.WriteString(strconv.Itoa(count))
+	result.WriteByte(s[len(s)-1])
+
+	return result.Bytes()
 }
