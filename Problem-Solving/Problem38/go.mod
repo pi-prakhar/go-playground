5861174a4eabf52acbad78b80380d8b50b0be7d9
Prakhar Awasthi <pawasthi961@gmail.com>
2024-08-06 23:02:48 +0530

    solved Longest Palindromic Substring

---
+++ Problem-Solving/Problem38/go.mod
@@ -0,0 +1,3 @@
+module problem38
+
+go 1.21.5
---
+++ Problem-Solving/Problem38/main.go
@@ -0,0 +1,35 @@
+package main
+
+func longestPalindrome(s string) string {
+	if len(s) == 1 {
+		return s
+	}
+	length := len(s)
+	for length > 0 {
+		start := 0
+		end := start + length
+		for end <= len(s) {
+			substring := s[start:end]
+			if isPalindrome(substring) {
+				return substring
+			}
+			start++
+			end++
+		}
+		length--
+	}
+	return s
+}
+
+func isPalindrome(s string) bool {
+	start := 0
+	end := len(s) - 1
+	for start <= end {
+		if s[start] != s[end] {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+}
