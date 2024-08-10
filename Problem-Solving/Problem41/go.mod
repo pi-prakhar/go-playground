1679a2b00ae8ab70c38984a96c01399fba2bf538
Prakhar Awasthi <pawasthi961@gmail.com>
2024-08-10 23:58:57 +0530

    solved maxSubArray

---
+++ Problem-Solving/Problem41/go.mod
@@ -0,0 +1,3 @@
+module problem41
+
+go 1.21.5
---
+++ Problem-Solving/Problem41/main.go
@@ -0,0 +1,18 @@
+package main
+
+func maxSubArray(nums []int) int {
+	tempSum := nums[0]
+	maxSum := nums[0]
+
+	for index := 1; index < len(nums); index++ {
+		tempSum = tempSum + nums[index]
+		if nums[index] > tempSum {
+			tempSum = nums[index]
+		}
+		if tempSum > maxSum {
+			maxSum = tempSum
+		}
+	}
+
+	return maxSum
+}
