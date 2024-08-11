1490779551c925fd77541e98b76bce3f6d4502e9
Prakhar Awasthi <pawasthi961@gmail.com>
2024-08-11 18:51:09 +0530

    solved Merge Intervals

---
+++ Problem-Solving/Problem44/go.mod
@@ -0,0 +1,3 @@
+module problem44
+
+go 1.21.5
---
+++ Problem-Solving/Problem44/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	var result [][]int
+	min := intervals[0][0]
+	max := intervals[0][1]
+	for index := 1; index < len(intervals); index++ {
+		start := intervals[index][0]
+		end := intervals[index][1]
+
+		if start > max {
+			result = append(result, []int{min, max})
+			min = start
+			max = end
+			continue
+		}
+
+		if (start <= max) && (end > max) {
+			max = end
+		}
+	}
+
+	result = append(result, []int{min, max})
+
+	return result
+}
