1a90028142cc6dcce831145ecdf884004eebb9fc
Prakhar Awasthi <pawasthi961@gmail.com>
2024-08-11 18:54:57 +0530

    solved set matrix zeroes

---
+++ Problem-Solving/Problem43/main.go
@@ -0,0 +1,25 @@
+package main
+
+func setZeroes(matrix [][]int) {
+	colLen := len(matrix[0])
+	var rows []int
+	var cols []int
+	for row := 0; row < len(matrix); row++ {
+		for col := 0; col < len(matrix[row]); col++ {
+			if matrix[row][col] == 0 {
+				rows = append(rows, row)
+				cols = append(cols, col)
+			}
+		}
+	}
+
+	for row := 0; row < len(rows); row++ {
+		matrix[rows[row]] = make([]int, colLen)
+	}
+	for col := 0; col < len(cols); col++ {
+		for row := 0; row < len(matrix); row++ {
+			matrix[row][cols[col]] = 0
+		}
+	}
+
+}
